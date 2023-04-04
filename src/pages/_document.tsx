bd12b4d05460f058c2def7c38af02ee9e36e1a5f
i-martello <martelloignacio@gmail.com>
2023-04-04 17:27:23 -0300

    cambios

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head
-       />
+      <link rel="icon" href="https://mi-portafolio-martello.vercel.app/_next/image?url=%2FLogo.png&w=96&q=75"></link>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
