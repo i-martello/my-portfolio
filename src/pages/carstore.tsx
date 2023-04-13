7736e17a671b8877ed08cd234462aa6ae12de586
i-martello <martelloignacio@gmail.com>
2023-04-13 08:05:34 -0300

    cambios

--- src/pages/carstore.tsx
+++ src/pages/carstore.tsx
@@ -43,6 +43,13 @@ const carstore = () => {
           >
             <button className="px-8 py-2 mt-4 mr-8">Código</button>
           </a>
+          <a
+            href="https://car-store-steel.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Ir a la página!</button>
+          </a>
         </div>
         <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
           <div className="p-2">
