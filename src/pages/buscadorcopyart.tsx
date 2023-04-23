40f8659a8e4201417b8ed056a0ba4e69200bbb4e
i-martello <martelloignacio@gmai.com>
2023-04-23 04:52:02 -0300

    cambios

--- src/pages/buscadorcopyart.tsx
+++ src/pages/buscadorcopyart.tsx
@@ -12,7 +12,7 @@ const buscadorcopyart = () => {
           className="absolute z-1"
           layout="fill"
           objectFit="cover"
-          src="/divertyweb.gif"
+          src="/buscadorCopyART.gif"
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
