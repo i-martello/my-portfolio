ac72a88542072be2ace2c8c6a274127685c6976e
i-martello <martelloignacio@gmail.com>
2024-02-11 04:04:31 -0300

    cambio

--- src/pages/buscadorcopyart.tsx
+++ src/pages/buscadorcopyart.tsx
@@ -17,7 +17,7 @@ const buscadorcopyart = () => {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
           <h2 className="py-2">Buscador CopyArt</h2>
-          <h3>Python / Pandas / NumPy / TypeScript / React</h3>
+          <h3>Python / Pandas / NumPy / TypeScript / React / MongoDB</h3>
         </div>
       </div>
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
@@ -65,6 +65,9 @@ const buscadorcopyart = () => {
               </p>
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> MongoDB
               </p>
             </div>
           </div>
